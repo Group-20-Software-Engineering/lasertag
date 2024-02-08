6ba79b3ef354663a9334002a171156c7a25aa7e9
coder061703 <blaker1703@gmail.com>
2024-02-08 16:36:58 -0600

    Testing: Asymetrical Communication ( ports and sockets)

--- udp/cpp_udp_client.cpp
+++ udp/cpp_udp_client.cpp
@@ -9,49 +9,68 @@
 
 
 
-const int SERVER_PORT = 25565;
+const int sendPort = 25565;
+const int receivePort = 8123;
 const int BUFFER_SIZE = 1024;
 
 int main() {
     const char* SERVER_IP = "170.176.232.159";
     // Create socket
-    int clientSocket = socket(AF_INET, SOCK_DGRAM, 0);
-    if (clientSocket == -1) {
+    int sendSocket = socket(AF_INET, SOCK_DGRAM, 0);
+    if (sendSocket == -1) {
         std::cerr << "Error creating socket" << std::endl;
         return 1;
     }
 
     // Set up server address
-    struct sockaddr_in serverAddress;
-    memset(&serverAddress, 0, sizeof(serverAddress));
-    serverAddress.sin_family = AF_INET;
-    serverAddress.sin_addr.s_addr = inet_addr(SERVER_IP);
-    serverAddress.sin_port = htons(SERVER_PORT);
-    char buffer[BUFFER_SIZE];
+    struct sockaddr_in sendAddress;
+    memset(&sendAddress, 0, sizeof(sendAddress));
+    sendAddress.sin_family = AF_INET;
+    sendAddress.sin_addr.s_addr = inet_addr(SERVER_IP);
+    sendAddress.sin_port = htons(sendPort);
+    char sendBuffer[BUFFER_SIZE];
+
+    //Create recive socket
+    int receiveSocket = socket(AF_INET, SOCK_DGRAM, 0);
+    if (receiveSocket == -1) {
+        std::cerr << "Error creating socket" << std::endl;
+        return 1;
+    }
+
+    char receiveBuffer[BUFFER_SIZE];
+    
 
     while (true) {
         // Get user input
         std::cout << "Enter a message (or type 'exit' to quit): ";
-        std::cin.getline(buffer, sizeof(buffer));
+        std::cin.getline(sendBuffer, sizeof(sendBuffer));
 
         // Check for exit command
-        if (strcmp(buffer, "exit") == 0) {
+        if (strcmp(sendBuffer, "exit") == 0) {
             break;
         }
 
         // Send the message to the server
-        ssize_t sentBytes = sendto(clientSocket, buffer, strlen(buffer), 0,
-                                   (struct sockaddr*)&serverAddress, sizeof(serverAddress));
+        ssize_t sentBytes = sendto(sendSocket, sendBuffer, strlen(sendBuffer), 0,
+                                   (struct sockaddr*)&sendAddress, sizeof(sendAddress));
         if (sentBytes == -1) {
             std::cerr << "Error sending data" << std::endl;
             continue;
         }
 
-        // Receive the response from the server (optional)
-        struct sockaddr_in serverResponseAddress;
-        socklen_t serverResponseAddrLen = sizeof(serverResponseAddress);
-        ssize_t receivedBytes = recvfrom(clientSocket, buffer, sizeof(buffer), 0,
-                                         (struct sockaddr*)&serverResponseAddress, &serverResponseAddrLen);
+        // // Receive the response from the server (optional)
+        // struct sockaddr_in receiveAddress;
+
+            // Set up server address
+        struct sockaddr_in receiveAddress;
+        memset(&receiveAddress, 0, sizeof(receiveAddress));
+        receiveAddress.sin_family = AF_INET;
+        receiveAddress.sin_addr.s_addr = inet_addr(SERVER_IP);
+        receiveAddress.sin_port = htons(receivePort);
+        
+        socklen_t receiveAddressLen = sizeof(receiveAddress);
+        ssize_t receivedBytes = recvfrom(receiveSocket, receiveBuffer, sizeof(receiveBuffer), 0,
+                                         (struct sockaddr*)&receiveAddress, &receiveAddressLen);
 
         if (receivedBytes == -1) {
             std::cerr << "Error receiving data" << std::endl;
@@ -59,12 +78,13 @@ int main() {
         }
 
         // Print the server's response
-        buffer[receivedBytes] = '\0'; // Null-terminate the received data
-        std::cout << "Server response: " << buffer << std::endl;
+        receiveBuffer[receivedBytes] = '\0'; // Null-terminate the received data
+        std::cout << "Server response: " << receiveBuffer << std::endl;
     }
 
     // Close the socket
-    close(clientSocket);
+    close(sendSocket);
+    close(receiveSocket);
 
     return 0;
 }
