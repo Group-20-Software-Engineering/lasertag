2ff905daae1b97037d0b541a2d4b6a76e3fd63b9
noahnew10 <noahnewman10@yahoo.com>
2024-02-08 15:49:36 -0600

    Added: main.py

---
+++ frontend/main.py
@@ -0,0 +1,14 @@
+#write a python front end for a laser tag game using the pygame library
+#this is the main file that will be run to start the game
+#this file will be responsible for setting up the game and running the game loop
+#this file will also be responsible for handling user input and updating the game state
+#this file will also be responsible for rendering the game state to the screen
+#this file will also be responsible for handling game events such as collisions and scoring
+#this file will also be responsible for handling game over conditions and ending the game
+#this file will also be responsible for handling game restarts and resets
+#this file will also be responsible for handling game exits and closing the game window
+#this file will also be responsible for handling game settings and options
+#this file will also be responsible for handling game audio and sound effects
+#this file will also be responsible for handling game graphics and visual effects
+#this file will also be responsible for handling game networking and multiplayer
+#this file will also be responsible for handling game saving and loading
