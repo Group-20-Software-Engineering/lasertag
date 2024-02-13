37720f4b6622c7f790bce42e914fad48b4c1e7e3
landonr7 <landon.reynolds777@gmail.com>
2024-02-13 15:25:24 -0600

    updated: finally communicating with database

--- backend/db.py
+++ backend/db.py
@@ -1,37 +1,35 @@
-import os
-from supabase import create_client, client
+from dotenv import load_dotenv
+load_dotenv()
 
-url: str = os.environ.get("https://jmfukmeanfezxzgrsitj.supabase.co")
-key: str = os.environ.get("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImptZnVrbWVhbmZlenh6Z3JzaXRqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDcyNTEyMDMsImV4cCI6MjAyMjgyNzIwM30.r99dqev77H1YPfAudZ9xm5heBt-jR-dNDiuI8-xVuZk")
+import os
+
+from supabase import create_client, Client
+
+url: str = os.environ.get("SUPABASE_URL")
+key: str = os.environ.get("SUPABASE_KEY")
+
 supabase: Client = create_client(url, key)
+
+data = supabase.table('player').insert({ 'id': 2, 'codename': 'Razor' }).execute()
+data = supabase.table('player').select("*").execute()
+
+print(data)
 
 """
 example data retrieval: 
-"response = supabase.table('player')
-    .select("*")
-    .execute()"
+"response = supabase.table('player').select("*").execute()"
 
 example data insertion: 
-"data, count = supabase.table('player')
-    .insert({ "id": 1, "codename": "77" })
-    .execute()"
+"data, count = supabase.table('player').insert({ "id": 1, "codename": "77" }).execute()"
 
 example data update: 
-"data, count = supabase.table('player')
-    .update({'codename': '69'})
-    .eq('id', 1)
-    .execute()"
+"data, count = supabase.table('player').update({'codename': '69'}).eq('id', 1).execute()"
 
 example data upsert (if present, then updates; if not present, then inserts):
-"data, count = supabase.table('player')
-    .upsert({'id': 1, 'codename': '50'})
-    .execute()"
+"data, count = supabase.table('player').upsert({'id': 1, 'codename': '50'}).execute()"
 
 example data deletion:
-"data, count = supabase.table('player')
-  .delete()
-  .eq('id', 1)
-  .execute()"
+"data, count = supabase.table('player').delete().eq('id', 1).execute()"
 
 """
 
