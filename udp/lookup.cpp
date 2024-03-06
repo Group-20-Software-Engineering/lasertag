4ed76cc613dbf22359a3de7b82f24cebe8b10b81
coder061703 <blaker1703@gmail.com>
2024-03-06 10:48:23 -0600

    Removed: Uneeded Binarys

--- udp/lookup.cpp
+++ udp/lookup.cpp
@@ -9,6 +9,7 @@ using ValueVariant = std::variant<int, std::string, double>;
 //Defines the Lookup table type
 using LookupTable = std::unordered_map<std::string, ValueVariant>;
 
+
 //add a key-value pair to the lookup table
 void lookuptableAdd(LookupTable& table, const std::string& key, const ValueVariant& value) {
     table[key] = value;
